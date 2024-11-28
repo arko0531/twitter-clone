5d3a061ed8a5a9a1f43a743e303dcb065d1cf42f
Ko Ara <132333218+arko0531@users.noreply.github.com>
2024-11-28 11:11:10 +0900

    feature/nwitter-3: Authentication

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCdzw0caHPMAOofOAS7keuhiWRifpynIdE',
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
